c2dd86cbdf89858db55f1bc9641df1b81960433b
mrangad007 <careandrroid@gmail.com>
2017-07-27 23:56:30 +0530

    Added Index.php & style.css & notes.txt

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Git Page</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<h2 class="c1">Hello Git</h2>
+</body>
+</html>
